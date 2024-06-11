0693a2409f74c522e95477b9591a9e2c931e9e01
elsawaf001 <elsawaf001@gmail.com>
2024-06-11 12:24:22 +0000

    g

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,12 @@
 'use server';
   
 import { z } from 'zod';
- 
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -19,11 +24,19 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
         // const rawFormData = {
         //   customerId: formData.get('customerId'),
         //   amount: formData.get('amount'),
         //   status: formData.get('status'),
         // };
-      
+
+        await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+
+      revalidatePath('/dashboard/invoices');
+      redirect('/dashboard/invoices');
 }
